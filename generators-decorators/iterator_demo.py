e7bb2c058c89ae244fd9b6aacf2f90f9fbc181ce
MdSaifulIslam <mohammadsaifulcsepstu@gmail.com>
2026-01-22 21:39:31 +0900

    - Generator

--- generators-decorators/iterator_demo.py
+++ generators-decorators/iterator_demo.py
@@ -1,25 +1,27 @@
 my_foods = ["apple", "banana", "cherry"]
 
-for food in my_foods:
-    for food2 in my_foods:
-        if food == food2:
-            print(f"Skipping duplicate food: {food}")
-            continue
-        print(f"Cooking {food} with {food2}")
+# for food in my_foods:
+#     for food2 in my_foods:
+#         if food == food2:
+#             print(f"Skipping duplicate food: {food}")
+#             continue
+#         print(f"Cooking {food} with {food2}")
 
 
 class CountTo:
-    def __init__(self, max_value):
+    def __init__(self, max_value, step):
         self.max = max_value
+        self.step = step
 
     def __iter__(self):
-        return CountToIterator(self.max)
+        return self# CountToIterator(self.max, self.step)
 
 
 class CountToIterator:
-    def __init__(self, max_value):
+    def __init__(self, max_value, step):
         self.max = max_value
         self.current = 1
+        self.step = step
 
     def __iter__(self):
         return self
@@ -27,13 +29,13 @@ class CountToIterator:
     def __next__(self):
         if self.current <= self.max:
             val = self.current
-            self.current += 1
+            self.current += self.step
             return val
         else:
             raise StopIteration
 
 
-counter = CountTo(5)
+counter = CountToIterator(10, 4)
 
 for count in counter:
     for count2 in counter:
